7cd2703eb1008a5a62acf60ee9def0f5ce1d9048
JDKWangGuan(街道口网管) <33923108+JiedaokouWangguan@users.noreply.github.com>
2019-05-05 18:25:53 -0500

    Update utils.py

--- easgd/utils.py
+++ easgd/utils.py
@@ -1,7 +1,6 @@
-from enum import Enum
 
 
-class MessageCode(Enum):
+class MessageCode(object):
     """Different types of messages between client and server that we support go here."""
     PullTilde = 0
     UpdateTilde = 1
