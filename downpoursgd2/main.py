8958dce1e049a037fb2d5cccfa50602cc6fc1334
JiedaokouWangguan <ysong243@wisc.edu>
2019-05-06 00:24:08 -0500

    ggggggggg

--- downpoursgd2/main.py
+++ downpoursgd2/main.py
@@ -30,9 +30,13 @@ class Net(nn.Module):
 def train(args, model, device, train_loader, optimizer, epoch):
     model.train()
     for batch_idx, (data, target) in enumerate(train_loader):
+
         data, target = data.to(device), target.to(device)
         optimizer.zero_grad()
         output = model(data)
+        print(data)
+        print(target)
+        print(output)
         # loss = F.nll_loss(output, target)
         loss = F.mse_loss(output, target)
         loss.backward()
