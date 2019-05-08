12e5fae61116cd589d283d7d5157f2a2cfb0bb3a
JDKWangGuan(街道口网管) <33923108+JiedaokouWangguan@users.noreply.github.com>
2019-05-08 15:35:58 -0500

    Update bandwidth.sh

--- bandwidth.sh
+++ bandwidth.sh
@@ -3,10 +3,7 @@
 function terminate_cluster() {
     echo "Terminating the servers"
 #    CMD="ps aux | grep -v 'grep' | grep 'python code_template' | awk -F' ' '{print $2}' | xargs kill -9"
-    CMD="ps aux | grep -v 'grep' | grep -v 'bash' | grep -v 'ssh' | grep 'python code_template' | awk -F' ' '{print \$2}' | xargs kill -9"
-    for i in `seq 0 2`; do
-        ssh node$i "$CMD"
-    done
+    ps aux | grep -v 'grep' | grep -v 'bash' | grep -v 'ssh' | grep 'python code_template' | awk -F' ' '{print \$2}' | xargs kill -9
 }
 
 terminate_cluster
