d5ee4cbdaaa7a957a845b9221efe2ed1c25a61f5
JiedaokouWangguan <ysong243@wisc.edu>
2019-05-08 11:45:24 -0500

    update metric shell

---
+++ easgd/latency.sh
@@ -0,0 +1,38 @@
+# usage run.sh {rank} {latency(xms)}
+
+function terminate_cluster() {
+    echo "Terminating the servers"
+#    CMD="ps aux | grep -v 'grep' | grep 'python code_template' | awk -F' ' '{print $2}' | xargs kill -9"
+    CMD="ps aux | grep -v 'grep' | grep -v 'bash' | grep -v 'ssh' | grep 'python code_template' | awk -F' ' '{print \$2}' | xargs kill -9"
+    for i in `seq 0 2`; do
+        ssh node$i "$CMD"
+    done
+}
+
+terminate_cluster
+start_time=$(date +%s)
+
+
+if [ $1 -eq 0 ] 
+then 
+    arg_ps="--flag"
+else 
+    arg_ps="--no-flag "                                                                
+fi
+
+sudo tc qdisc add dev eno1 root netem delay $2 
+
+dstat -n > network.csv &
+python main.py --world-size 3 --rank $1 --flag $arg_ps 'tcp://node0:8088' --quantize-nbits 8;
+
+sudo tc qdisc del dev eno1 root netem delay $2 
+
+
+
+end_time=$(date +%s)
+diff=$(( $end_time - $start_time ))
+echo "It took $diff seconds"
+terminate_cluster
+
+
+
