dc034e7daa8664335b286b83664d5053e1a4855e
JiedaokouWangguan <ysong243@wisc.edu>
2019-05-09 10:40:41 -0500

    debug

--- downpoursgd2/utils.py
+++ downpoursgd2/utils.py
@@ -1,4 +1,5 @@
 import torch
+from struct import pack, unpack
 
 
 class MessageCode(object):
@@ -8,7 +9,7 @@ class MessageCode(object):
     WorkerTerminate = 2
 
 
-def quantize_tensor(x, num_bits=8):
+def quantize_tensor(x, num_bits):
     qmin = 0.
     qmax = 2.**num_bits - 1.
     min_val, max_val = x.min(), x.max()
@@ -27,16 +28,38 @@ def quantize_tensor(x, num_bits=8):
     zero_point = int(zero_point)
     q_x = zero_point + x / scale
     q_x.clamp_(qmin, qmax).round_()
-    q_x = q_x.round()
 
-    m_parameter = torch.Tensor([scale, zero_point])
-    m_parameter = torch.cat((m_parameter, q_x))
+    b = pack('f', scale)
+    b = pack('f', scale)
+    c = unpack('i', b)[0]
+    x1 = 0x000000FF & c
+    x2 = (0X0000FF00 & c) >> 8
+    x3 = (0x00FF0000 & c) >> 16
+    x4 = (0xFF000000 & c) >> 24
+
+    m_parameter = torch.Tensor([x1, x2, x3, x4, zero_point])
+    m_parameter = torch.cat((q_x, m_parameter))
+
+    m_parameter = m_parameter.round().byte()
 
     return m_parameter
 
 
 def dequantize_tensor(m_parameter):
-    scale = float(m_parameter[0].item())
-    zero_point = int(m_parameter[1].item())
-    q_x = m_parameter[2:]
+
+    scale1 = int(m_parameter[-5].item())
+    scale2 = int(m_parameter[-4].item())
+    scale3 = int(m_parameter[-3].item())
+    scale4 = int(m_parameter[-2].item())
+
+    scale = 0
+    scale = scale1 | scale
+    scale = (scale2 << 8) | scale
+    scale = (scale3 << 16) | scale
+    scale = (scale4 << 24) | scale
+
+    scale = pack('I', scale)
+    scale = unpack('f', scale)[0]
+    zero_point = int(m_parameter[-1].item())
+    q_x = m_parameter[:-5]
     return scale * (q_x.float() - zero_point)
