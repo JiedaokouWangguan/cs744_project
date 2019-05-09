1d58c3135ee7266f8df593d9d7e524d6628a9872
JiedaokouWangguan <ysong243@wisc.edu>
2019-05-09 14:58:29 -0500

    updagte

--- latency.sh
+++ latency.sh
@@ -1,4 +1,4 @@
-# usage run.sh {rank} {latency(xms)} {downpoursgd2|easgd}
+# usage run.sh {rank} {latency(xms)} {downpoursgd2|easgd} {quantize-nbits}
 
 function terminate_cluster() {
     echo "Terminating the servers"
@@ -21,7 +21,7 @@ sudo tc qdisc add dev eno1 root netem delay $2
 
 dstat -n > network_$3_$2.csv &
 cd ../
-python -m cs744_project.$3.main --world-size 3 --rank $1 $arg_ps --dist-url 'tcp://node0:8088' --quantize-nbits 8;
+python -m cs744_project.$3.main --world-size 3 --rank $1 $arg_ps --dist-url 'tcp://node0:8088' --quantize-nbits $4;
 cd ./cs744_project
 sudo tc qdisc del dev eno1 root netem delay $2 
 
